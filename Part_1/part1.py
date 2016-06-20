13a1989a82644d90fd19f5cf3d7d6607fa19a901
Roman Kulak <rome1407@gmail.com>
2016-06-20 14:22:33 +0300

    correction commit

--- Part_1/part1.py
+++ Part_1/part1.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-# Function generates 10*10*10 array with random integers [0;9]
 def random_integers(low=0, high=9, elements=10, dimension=3):
+    """Function generates 10*10*10 array with random integers [0;9]"""
     arr3d = np.random.randint(low, high=high, size=elements**dimension)
     return arr3d.reshape((elements,)*3)
 
 vec = random_integers() # default 10*10*10 array with random integers [0;9]
 
-# Function returns the maximum sum of columns and columns coordinates
 def max_sum_coord(vec, n):
+    """Function returns the maximum sum of columns and columns coordinates"""
     coordinates=[]
     all_sum=[]
     for matrix in xrange(n):
@@ -26,8 +26,8 @@ def max_sum_coord(vec, n):
 n=vec.shape[0]
 max_sum,coordinates=max_sum_coord(vec, n)
 
-# Returns 3 perpendicular columns ( numpy arrays)  which sum is the maximum
 def max_columns(vec, coordinates):
+    """Returns 3 perpendicular columns ( numpy arrays)  which sum is the maximum"""
     matrix,column,row=coordinates[0],coordinates[1],coordinates[2]
     vectors = []
     return vec[matrix,:,column], vec[matrix,row,:], vec[:,row,column]
